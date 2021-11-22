b6d8c21fb72731419834f8026142669dab39656c
Axel Drudi <axel.drudi@studio.unibo.it>
2021-11-22 12:53:36 +0100

    Implement both "ConcurrentGUI.java" in the first two "reactivegui" packages and set up "Test.java" in the second one

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -1,5 +1,4 @@
-package it.unibo.oop.lab.reactivegui01;
-
+package it.unibo.oop.lab.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -13,15 +12,14 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-/**
- * This is a first example on how to realize a reactive GUI.
- */
-public final class ConcurrentGUI extends JFrame {
+public class ConcurrentGUI extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private static final double WIDTH_PERC = 0.2;
     private static final double HEIGHT_PERC = 0.1;
-    private final JLabel display = new JLabel();
+    private final JLabel display = new JLabel("0");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
     private final JButton stop = new JButton("stop");
 
     /**
@@ -34,13 +32,13 @@ public final class ConcurrentGUI extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         final JPanel panel = new JPanel();
         panel.add(display);
+        panel.add(up);
+        panel.add(down);
         panel.add(stop);
         this.getContentPane().add(panel);
         this.setVisible(true);
         /*
-         * Create the counter agent and start it. This is actually not so good:
-         * thread management should be left to
-         * java.util.concurrent.ExecutorService
+         * Create the counter agent and start it.
          */
         final Agent agent = new Agent();
         new Thread(agent).start();
@@ -58,6 +56,39 @@ public final class ConcurrentGUI extends JFrame {
             public void actionPerformed(final ActionEvent e) {
                 // Agent should be final
                 agent.stopCounting();
+                up.setEnabled(false);
+                down.setEnabled(false);
+                stop.setEnabled(false);
+            }
+        });
+        /*
+         * Register a listener that make it increment
+         */
+        up.addActionListener(new ActionListener() {
+            /**
+             * event handler associated to action event on button up.
+             * 
+             * @param e
+             *            the action event that will be handled by this listener
+             */
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                agent.setInc();                
+            }
+        });
+        /*
+         * Register a listener that make it decrement
+         */
+        down.addActionListener(new ActionListener() {
+            /**
+             * event handler associated to action event on button down.
+             * 
+             * @param e
+             *            the action event that will be handled by this listener
+             */
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                agent.setDec();                
             }
         });
     }
@@ -67,54 +98,40 @@ public final class ConcurrentGUI extends JFrame {
      * invisible outside and encapsulated.
      */
     private class Agent implements Runnable {
-        /*
-         * Stop is volatile to ensure visibility. Look at:
-         * 
-         * http://archive.is/9PU5N - Sections 17.3 and 17.4
-         * 
-         * For more details on how to use volatile:
-         * 
-         * http://archive.is/4lsKW
-         * 
-         */
+
         private volatile boolean stop;
-        private int counter;
+        private volatile boolean up = true;
+        private volatile int counter;
 
         @Override
         public void run() {
             while (!this.stop) {
                 try {
-                    /*
-                     * All the operations on the GUI must be performed by the
-                     * Event-Dispatch Thread (EDT)!
-                     */
-                    final int count = this.counter; 
                     SwingUtilities.invokeAndWait(new Runnable() {
                         @Override
                         public void run() {
-                            // This will happen in the EDT: since i'm reading counter it needs to be volatile.
-                            ConcurrentGUI.this.display.setText(Integer.toString(count));
+                            ConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter));
                         }
                     });
-                    /*
-                     * SpotBugs shows a warning because the increment of a volatile variable is not atomic,
-                     * so the concurrent access is potentially not safe. In the specific case of this exercise,
-                     * we do synchronization with invokeAndWait, so it can be ignored.
-                     *
-                     * EXERCISE: Can you think of a solution that doesn't require counter to be volatile?
-                     */
-                    this.counter++;
+                    if (up) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
                     Thread.sleep(100);
                 } catch (InvocationTargetException | InterruptedException ex) {
-                    /*
-                     * This is just a stack trace print, in a real program there
-                     * should be some logging and decent error reporting
-                     */
                     ex.printStackTrace();
                 }
             }
         }
 
+        public void setInc() {
+            this.up = true;
+        }
+
+        public void setDec() {
+            this.up = false;
+        }
         /**
          * External command to stop counting.
          */
